42b2fe1a24020a1484a45456fa75c8a857650d2e
habib21-7 <habeebalsheikh8@gmail.com>
2023-11-02 15:15:00 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import './App.css';
+import React, { useState } from 'react';
 import ToDo from './components/ToDo/ToDo';
+import './App.css';
 
-function App() {
+const App = () => {
+  const [todoLists, setTodoLists] = useState([<ToDo key={0} />]);
+
+  const addTodoList = () => {
+    const newList = <ToDo key={todoLists.length} />;
+    setTodoLists([...todoLists, newList]);
+  };
+
   return (
-    <>
-      <ToDo />
-    </>
+    <div className="App">
+      <h1>ToDo Lists</h1>
+      <div className="todo-list-container">
+        {todoLists}
+      </div>
+      <br />
+      <button onClick={addTodoList}>Add ToDo List</button>
+    </div>
   );
 }
 
--- src/components/ToDo/ToDo.js
+++ src/components/ToDo/ToDo.js
@@ -1,14 +1,77 @@
+import React from 'react';
 import { useState } from 'react';
-
 
 const ToDo = () => {
 
-    return (
-        <>
-            <h3>ToDo</h3>
+  const [toDoTasksList, setToDoTasksList] = useState([]);
+  const [selectedTask, setSelectedTask] = useState(null);
+  const [editingTask, setEditingTask] = useState(null);
+  const [newTask, setNewTask] = useState('');
 
-        </>
-    );
+
+  const addTask = (task) => {
+    setToDoTasksList([...toDoTasksList,{ 
+        id: Date.now(), 
+        text: task 
+    }]);
+    setNewTask('');
+  };
+
+  const editTask = (updatedText) => {
+    const updatedToDoTasksList = toDoTasksList.map((task) => {
+      if (task.id === selectedTask) {
+        return { ...task, text: updatedText };
+      }
+      return task;
+    });
+    setToDoTasksList(updatedToDoTasksList);
+    setSelectedTask(null);
+    setEditingTask(null);
+  };
+
+  const deleteTask = (taskID) => {
+    const updatedToDoTasksList = toDoTasksList.filter((task) => task.id !== taskID);
+    setToDoTasksList(updatedToDoTasksList);
+  };
+
+  return (
+    <div>
+      <h2>ToDo List</h2>
+      <ul>
+        {toDoTasksList.map((task) => (
+          <li key={task.id}>
+            <div>
+              {selectedTask === task.id ? (
+                <div>
+                  <input
+                    type="text"
+                    value={editingTask !== null ? editingTask : task.text}
+                    onChange={(e) => setEditingTask(e.target.value)}
+                  />
+                  <button onClick={() => editTask(editingTask)}>Update</button>
+                </div>
+              ) : (
+                <div>
+                  <span>{task.text}</span>
+                  <button onClick={() => setSelectedTask(task.id)}>Edit</button>
+                  <button onClick={() => deleteTask(task.id)}>Delete</button>
+                </div>
+              )}
+            </div>
+          </li>
+        ))}
+      </ul>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter a new task"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+        />
+        <button onClick={() => addTask(newTask)}>Add Task</button>
+      </div>
+    </div>
+  );
 }
 
 export default ToDo;
